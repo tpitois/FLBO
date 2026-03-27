69287d55c924ead71efe5fd0f30f73628c5fd4c1
tpitois <tom.pitois@protonmail.com>
2026-03-27 23:30:22 +0100

    [edit] LayerNorm instead of BatchNorm

--- src/models/ascnn.py
+++ src/models/ascnn.py
@@ -11,17 +11,17 @@ class ACSCNN(nn.Module):
         self.n_class = n_class
 
         self.conv1 = ACSConv(n_desc, 64)
-        self.bn1 = nn.BatchNorm1d(64)
+        self.bn1 = nn.LayerNorm(64)
         self.conv2 = ACSConv(64, 64)
-        self.bn2 = nn.BatchNorm1d(64)
+        self.bn2 = nn.LayerNorm(64)
         self.conv3 = ACSConv(64, 64)
-        self.bn3 = nn.BatchNorm1d(64)
+        self.bn3 = nn.LayerNorm(64)
         self.conv4 = ACSConv(64, 64)
-        self.bn4 = nn.BatchNorm1d(64)
+        self.bn4 = nn.LayerNorm(64)
         self.conv5 = ACSConv(64, 64)
-        self.bn5 = nn.BatchNorm1d(64)
+        self.bn5 = nn.LayerNorm(64)
         self.conv6 = ACSConv(64, 64)
-        self.bn6 = nn.BatchNorm1d(64)
+        self.bn6 = nn.LayerNorm(64)
         self.fc2 = nn.Linear(64, 256)
         self.fc3 = nn.Linear(256, n_class)
 
